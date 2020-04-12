c2c8d5226ed6bfa7cb1b75badf2c2fc9e1751a48
root <root@itc.us-central1-a.c.skilled-text-266903.internal>
2020-04-12 22:51:21 +0000

    actualizo product.base64.csv

--- productos/MakeBase64CSV.sh
+++ productos/MakeBase64CSV.sh
@@ -8,7 +8,7 @@ echo \"External ID\",\"Image\" > $2
 while IFS=";" read f1 f2 f3; do
 
 # recopy external ID
-echo -n $f1; >> $2
+echo -n $f1, >> $2
 
 #If second  column represents the key to match with the filename, please use this command
 cat $(echo ${f3} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
