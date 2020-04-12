505ab3e98a9336d5072a880c01b2d6dca6a7de2c
root <root@itc.us-central1-a.c.skilled-text-266903.internal>
2020-04-12 22:41:38 +0000

    actualizo product.base64.csv

--- productos/MakeBase64CSV.sh
+++ productos/MakeBase64CSV.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-# Usage : MakeBase64CSV.sh infile.csv outfile.csv
-# infile.csv columns are : externalID, filename or identifier
-# infile.csv separator MUST BE |
+# Usage : MakeBase64CSV.sh product_imagen.csv prodcut_base64.csv
+# product_imagen.csv columns are : externalID, nombre, filename or identifier
+# product_imagen.csv separator MUST BE |
 
 echo \"External ID\",\"Image\" > $2
 
-while IFS="|" read f1 f2; do
+while IFS=";" read f1 f2 f3; do
 
 # recopy external ID
 echo -n $f1, >> $2
 
 #If second  column represents the key to match with the filename, please use this command
-cat $(echo ${f2} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
+cat $(echo ${f3} | tr -d '\r' | tr -d '"').jpg | base64 --wrap=0 >> $2
 
 #Carrier return at end of line
 echo  >> $2
 done < $1
-
