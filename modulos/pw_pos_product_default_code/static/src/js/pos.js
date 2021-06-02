e46e2a009270380b448ec3498c3017c0675169ee
JJARAMIF <jamiljaramillo@Jamils-MacBook-Pro.local>
2021-06-01 23:50:20 -0500

    pw_pos_product_default_code

---
+++ modulos/pw_pos_product_default_code/static/src/js/pos.js
@@ -0,0 +1,20 @@
+odoo.define('pos_default_code.pos_default_code', function (require) {
+    "use strict";
+        var gui = require('point_of_sale.gui');
+    var models = require('point_of_sale.models');
+    var screens = require('point_of_sale.screens');
+    var PopupWidget = require('point_of_sale.popups');
+    var core = require('web.core');
+
+    var _t = core._t;
+    var _super_orderline = models.Orderline.prototype;
+    models.Orderline = models.Orderline.extend({
+        generate_wrapped_product_name: function() {
+        	var name = _super_orderline.generate_wrapped_product_name.call(this);
+        	if (this.get_product().default_code) {
+        		name[0] = '['+this.get_product().default_code + '] ' + name[0]
+        	}
+        	return name
+        },
+    });
+});
